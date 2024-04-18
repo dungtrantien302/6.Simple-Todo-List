9243af3a502fc0753d9d2be2035e86e68aa102cd
Tran Tien Dung <dungtrantien302@gmail.com>
2024-04-18 19:13:12 +0700

    Edit file App.jsx, Todo.jsx to Add the ability to mark a todo as complete

--- vite/src/Todo.jsx
+++ vite/src/Todo.jsx
@@ -1,6 +1,20 @@
 //1 Add new todo to list todo
+// export function Todo({ newtodo }) {
+//     return (
+//         <li> {newtodo}</li>
+//     )
+// }
+
+//2 Add the ability to mark a todo as complete 
 export function Todo({ newtodo }) {
     return (
-        <li> {newtodo}</li>
+        <ul id="list">
+            <li className="list-item">
+                <label className="list-item-label">
+                    <input type="checkbox" data-list-item-checkbox />
+                    <span data-list-item-text>{newtodo}</span>
+                </label>
+            </li>
+        </ul>
     )
 }
