3f602b68713a7f9fc22f566221184d36e533c8a1
Tran Tien Dung <dungtrantien302@gmail.com>
2024-04-18 19:23:43 +0700

    Edit file App.jsx, Todo.jsx to Add a delete button next to each todo

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -47,6 +47,54 @@
 
 
 //2 Add the ability to mark a todo as complete 
+// import { useState } from 'react'
+// import './styles.css'
+// import { Todo } from './Todo'
+
+// export default function App() {
+//   const [todoInput, setTodoInput] = useState('')
+//   const [todoProces, setTodoProces] = useState({})
+//   const [listTodos, setListTodos] = useState([])
+//   let [idTodo, setIdTodo] = useState(0)
+
+//   function addNewTodo(addTodo) {
+//     if (todoInput !== '') {
+//       setIdTodo(currentIdTodo => {
+//         return currentIdTodo + 1
+//       })
+
+//       todoProces.name = addTodo
+//       todoProces.id = idTodo
+
+//       setListTodos(currentListTodos => {
+//         return [...currentListTodos, todoProces]
+//       })
+
+//       setTodoProces({})
+//       setTodoInput('')
+//     }
+//   }
+
+//   return (
+//     <>
+//       <ul>
+//         {listTodos.map(todo => {
+//           return <Todo key={todo.id} newtodo={todo.name} />
+//         })}
+//       </ul>
+
+//       <div id="new-todo-form">
+//         <label htmlFor="todo-input" id='todo-input'>New Todo</label>
+//         <input type="text" id="todo-input" value={todoInput} onChange={e => setTodoInput(e.target.value)} />
+//         <button onClick={() => addNewTodo(todoInput)}>Add Todo</button>
+//       </div>
+
+//     </>
+//   )
+// }
+
+
+//3 Add a delete button next to each todo
 import { useState } from 'react'
 import './styles.css'
 import { Todo } from './Todo'
@@ -75,11 +123,17 @@ export default function App() {
     }
   }
 
+  function deleteTodo(idTodo) {
+    setListTodos(currentListTodos => {
+      return currentListTodos.filter(todo => todo.id !== idTodo)
+    })
+  }
+
   return (
     <>
       <ul>
         {listTodos.map(todo => {
-          return <Todo key={todo.id} newtodo={todo.name} />
+          return <Todo key={todo.id} newtodo={todo.name} idTodo={todo.id} deleteTodo={deleteTodo} />
         })}
       </ul>
 
--- vite/src/Todo.jsx
+++ vite/src/Todo.jsx
@@ -6,13 +6,28 @@
 // }
 
 //2 Add the ability to mark a todo as complete 
-export function Todo({ newtodo }) {
+// export function Todo({ newtodo }) {
+//     return (
+//         <ul id="list">
+//             <li className="list-item">
+//                 <label className="list-item-label">
+//                     <input type="checkbox" data-list-item-checkbox />
+//                     <span data-list-item-text>{newtodo}</span>
+//                 </label>
+//             </li>
+//         </ul>
+//     )
+// }
+
+//3 Add a delete button next to each todo
+export function Todo({ newtodo, idTodo, deleteTodo }) {
     return (
         <ul id="list">
             <li className="list-item">
                 <label className="list-item-label">
                     <input type="checkbox" data-list-item-checkbox />
                     <span data-list-item-text>{newtodo}</span>
+                    <button data-button-delete onClick={() => deleteTodo(idTodo)}>Delete</button>
                 </label>
             </li>
         </ul>
