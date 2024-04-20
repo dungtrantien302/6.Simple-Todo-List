02004ae33c3a0e7ce46417e6dc7e2dd9e0f8a6b3
Tran Tien Dung <dungtrantien302@gmail.com>
2024-04-20 07:15:05 +0700

    Edit file App.jsx, Todo.jsx to completed

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,148 +1,48 @@
-//1 Add new todo to list todo
-// import { useState } from 'react'
-// import './styles.css'
-// import { Todo } from './Todo'
-
-// export default function App() {
-//   const [todoInput, setTodoInput] = useState('')
-//   const [todoProces, setTodoProces] = useState({})
-//   const [listTodos, setListTodos] = useState([])
-//   let [idTodo, setIdTodo] = useState(0)
-
-//   function addNewTodo(addTodo) {
-//     if (todoInput !== '') {
-//       setIdTodo(currentIdTodo => {
-//         return currentIdTodo + 1
-//       })
-
-//       todoProces.name = addTodo
-//       todoProces.id = idTodo
-
-//       setListTodos(currentListTodos => {
-//         return [...currentListTodos, todoProces]
-//       })
-
-//       setTodoProces({})
-//       setTodoInput('')
-//     }
-//   }
-
-//   return (
-//     <>
-//       <ul>
-//         {listTodos.map(todo => {
-//           return <Todo key={todo.id} newtodo={todo.name} />
-//         })}
-//       </ul>
-
-//       <div id="new-todo-form">
-//         <label htmlFor="todo-input" id='todo-input'>New Todo</label>
-//         <input type="text" id="todo-input" value={todoInput} onChange={e => setTodoInput(e.target.value)} />
-//         <button onClick={() => addNewTodo(todoInput)}>Add Todo</button>
-//       </div>
-
-//     </>
-//   )
-// }
-
-
-//2 Add the ability to mark a todo as complete 
-// import { useState } from 'react'
-// import './styles.css'
-// import { Todo } from './Todo'
-
-// export default function App() {
-//   const [todoInput, setTodoInput] = useState('')
-//   const [todoProces, setTodoProces] = useState({})
-//   const [listTodos, setListTodos] = useState([])
-//   let [idTodo, setIdTodo] = useState(0)
-
-//   function addNewTodo(addTodo) {
-//     if (todoInput !== '') {
-//       setIdTodo(currentIdTodo => {
-//         return currentIdTodo + 1
-//       })
-
-//       todoProces.name = addTodo
-//       todoProces.id = idTodo
-
-//       setListTodos(currentListTodos => {
-//         return [...currentListTodos, todoProces]
-//       })
-
-//       setTodoProces({})
-//       setTodoInput('')
-//     }
-//   }
-
-//   return (
-//     <>
-//       <ul>
-//         {listTodos.map(todo => {
-//           return <Todo key={todo.id} newtodo={todo.name} />
-//         })}
-//       </ul>
-
-//       <div id="new-todo-form">
-//         <label htmlFor="todo-input" id='todo-input'>New Todo</label>
-//         <input type="text" id="todo-input" value={todoInput} onChange={e => setTodoInput(e.target.value)} />
-//         <button onClick={() => addNewTodo(todoInput)}>Add Todo</button>
-//       </div>
-
-//     </>
-//   )
-// }
-
-
-//3 Add a delete button next to each todo
 import { useState } from 'react'
 import './styles.css'
 import { Todo } from './Todo'
 
 export default function App() {
   const [todoInput, setTodoInput] = useState('')
-  const [todoProces, setTodoProces] = useState({})
-  const [listTodos, setListTodos] = useState([])
-  let [idTodo, setIdTodo] = useState(0)
+  const [todos, setTodos] = useState([])
+  const [idTodo, setIdTodo] = useState(0)
 
-  function addNewTodo(addTodo) {
+  function addNewTodo() {
     if (todoInput !== '') {
       setIdTodo(currentIdTodo => {
         return currentIdTodo + 1
       })
 
-      todoProces.name = addTodo
+      const todoProces = {}
+
+      todoProces.name = todoInput
       todoProces.id = idTodo
 
-      setListTodos(currentListTodos => {
-        return [...currentListTodos, todoProces]
+      setTodos(currentTodos => {
+        return [...currentTodos, todoProces]
       })
 
-      setTodoProces({})
       setTodoInput('')
     }
   }
 
-  function deleteTodo(idTodo) {
-    setListTodos(currentListTodos => {
-      return currentListTodos.filter(todo => todo.id !== idTodo)
+  function handDeleteTodo(idTodo) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== idTodo)
     })
   }
 
   return (
     <>
       <ul>
-        {listTodos.map(todo => {
-          return <Todo key={todo.id} newtodo={todo.name} idTodo={todo.id} deleteTodo={deleteTodo} />
-        })}
+        {todos.map(todo => <Todo key={todo.id} name={todo.name} idTodo={todo.id} handDeleteTodo={handDeleteTodo} />)}
       </ul>
 
       <div id="new-todo-form">
-        <label htmlFor="todo-input" id='todo-input'>New Todo</label>
+        <label htmlFor="todo-input">New Todo</label>
         <input type="text" id="todo-input" value={todoInput} onChange={e => setTodoInput(e.target.value)} />
-        <button onClick={() => addNewTodo(todoInput)}>Add Todo</button>
+        <button onClick={addNewTodo}>Add Todo</button>
       </div>
-
     </>
   )
 }
--- vite/src/Todo.jsx
+++ vite/src/Todo.jsx
@@ -1,33 +1,11 @@
-//1 Add new todo to list todo
-// export function Todo({ newtodo }) {
-//     return (
-//         <li> {newtodo}</li>
-//     )
-// }
-
-//2 Add the ability to mark a todo as complete 
-// export function Todo({ newtodo }) {
-//     return (
-//         <ul id="list">
-//             <li className="list-item">
-//                 <label className="list-item-label">
-//                     <input type="checkbox" data-list-item-checkbox />
-//                     <span data-list-item-text>{newtodo}</span>
-//                 </label>
-//             </li>
-//         </ul>
-//     )
-// }
-
-//3 Add a delete button next to each todo
-export function Todo({ newtodo, idTodo, deleteTodo }) {
+export function Todo({ name, idTodo, handDeleteTodo }) {
     return (
         <ul id="list">
             <li className="list-item">
                 <label className="list-item-label">
                     <input type="checkbox" data-list-item-checkbox />
-                    <span data-list-item-text>{newtodo}</span>
-                    <button data-button-delete onClick={() => deleteTodo(idTodo)}>Delete</button>
+                    <span data-list-item-text>{name}</span>
+                    <button data-button-delete onClick={() => handDeleteTodo(idTodo)}>Delete</button>
                 </label>
             </li>
         </ul>
